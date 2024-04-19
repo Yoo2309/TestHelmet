45e145cc1dc56d73087ddc0282364d4b612081c4
Yoo2309 <dutran2392002@gmail.com>
2024-04-19 22:17:24 +0700

    update

--- src/App.js
+++ src/App.js
@@ -13,10 +13,10 @@ function App() {
             path="/1"
             element={
               <HelmetComponent1
+                imageUrl={"http://i.imgur.com/T2hGPCv.png"}
                 description={"đây là component 1"}
                 url={"https://test-helmet-eight.vercel.app/1"}
                 title={"Trang chủ"}
-                imageUrl={"https://i.imgur.com/d2waE9z.png"}
                 imageAlt={"lỗi ảnh"}
               />
             }
@@ -26,12 +26,10 @@ function App() {
             path="/2"
             element={
               <HelmetComponent2
+                imageUrl={"http://i.imgur.com/o4d1BnE.png"}
                 description={"đây là component 2"}
-                url={
-                  "https://test-helmet-eight.vercel.app/2"
-                }
+                url={"https://test-helmet-eight.vercel.app/2"}
                 title={"Trang chi tiết"}
-                imageUrl={"https://i.imgur.com/OxfKelV.png"}
                 imageAlt={"lỗi ảnh"}
               />
             }
