0d865e0d880b021874a417de2c8451cbf4c4d85b
Yoo2309 <dutran2392002@gmail.com>
2024-04-19 22:35:19 +0700

    update

--- src/MetaDecor.jsx
+++ src/MetaDecor.jsx
@@ -3,7 +3,8 @@ import PropTypes from "prop-types";
 import { Helmet } from "react-helmet";
 
 const metaDecorator = {
-  twitterUsername: "@celikkoseoglu",
+  "hostname": "https://test-helmet-eight.vercel.app/  ",
+  "twitterUsername": "@celikkoseoglu"
 };
 
 const MetaDecorator = ({ title, description, imageUrl, imageAlt, url }) => (
@@ -12,7 +13,7 @@ const MetaDecorator = ({ title, description, imageUrl, imageAlt, url }) => (
     <meta property="og:title" content={title} />
     <meta name="description" content={description} />
     <meta property="og:description" content={description} />
-    <meta property="og:image" content={imageUrl} />
+    <meta property="og:image" content={metaDecorator.hostname + imageUrl} />
     <meta property="og:url" content={url} />
     <meta name="twitter:card" content="summary_large_image" />
     <meta name="twitter:image:alt" content={imageAlt} />
