8494274c76eb9f7fbb1da17dc33b13e5d13a2247
Yoo2309 <dutran2392002@gmail.com>
2024-04-27 02:45:38 +0700

    update

--- src/HelmetComponent1.jsx
+++ src/HelmetComponent1.jsx
@@ -1,21 +1,24 @@
 import React from "react";
 import { Helmet } from "react-helmet";
-import img from "./asserts/previewWebImg.png"
+import img from "./asserts/previewWebImg.png";
 
 function HelmetComponent1() {
   return (
     <div>
       <Helmet>
-        <meta property="og:image" content={img} />
+        <meta property="og:image" content="http://i.imgur.com/o4d1BnE.png" />
         <title>{"Component 1"}</title>
         <meta property="og:title" content={"Component 1"} />
         <meta name="description" content={"Đay là component 1"} />
         <meta property="og:description" content={"Đay là component 1"} />
-        <meta property="og:url" content={"https://test-helmet-eight.vercel.app/1"} />
+        <meta
+          property="og:url"
+          content={"https://test-helmet-eight.vercel.app/1"}
+        />
         <meta name="twitter:card" content="summary_large_image" />
         <meta name="twitter:image:alt" content={"anhr 1"} />
       </Helmet>
-      );
+      
       <h2>COMPONENT 1</h2>
     </div>
   );
